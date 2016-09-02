c0d6323b391575a7ada1928a960c16c260642c57
landodewd <lando.embry@gmail.com>
2016-09-02 09:46:26 -0600

    move to ubuntu1

---
+++ Javascript/functions.js
@@ -0,0 +1,18 @@
+
+var getDieRoll = function(dieSize) {
+	var result = Math.ceil(dieSize * Math.random());
+	return result;
+};
+
+var showResult = function() {
+	console.log(firstDie);
+console.log(secondDie);
+console.log(firstDie + secondDie);
+}
+
+var firstDie = getDieRoll(10);
+var secondDie = getDieRoll(10);
+showResult();
+//
+//Simulate a roll of the dice
+//
--- Javascript/variables.js
+++ Javascript/variables.js
@@ -1,12 +1,20 @@
 var x = 3;
-var y = 2;
+var y = 2.5;
 
-x = 3;
-console.log(x);
+var z= "3";
+var result = x > y;
+var writeGreeting = function(name){
+	var message = "Hello, " + name;
+	console.log(message);
+}
+writeGreeting("Landon");
+writeGreeting("Embry");
 
-console.log(x);
-
-x = 5;
-console.log(x);
-
-console.log(y);
+var answer = parseInt(z) + y;
+console.log(answer);
+console.log(typeof answer);
+console.log(typeof result); 
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof console);
+console.log(typeof console.log);
